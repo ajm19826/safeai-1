59a8a192a608f242f8408652b27ce10a1defaad1
ajm19826 <cajm23331@gmail.com>
2026-02-15 17:11:01 -0500

    start over

--- backend/reinforcement.py
+++
@@ -1,9 +0,0 @@
-class Reinforcement:
-    def __init__(self):
-        self.rewards = 0
-
-    def reward(self):
-        self.rewards += 1
-
-    def punish(self):
-        self.rewards -= 1
--- backend/utils.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def tokenize(text):
-    return re.findall(r"\d+|\w+|[\+\-\*/]", text.lower())
-
-def is_math(tokens):
-    return any(t in "+-*/" for t in tokens)
